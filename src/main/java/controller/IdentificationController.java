87b5d4727fab6b79ab6c5e588dfbc423879f5063
mjp81 <mjp81@LAPTOP-2N0EMT47>
2020-04-20 18:25:15 +0200

    Identification controller

--- src/main/java/controller/IdentificationController.java
+++ src/main/java/controller/IdentificationController.java
@@ -5,8 +5,18 @@
  */
 package controller;
 
+import comptoirs.model.dao.ClientFacade;
+import comptoirs.model.entity.Client;
+import java.util.List;
+import javax.inject.Inject;
 import javax.mvc.Controller;
+import javax.mvc.Models;
 import javax.mvc.View;
+import javax.validation.executable.ExecutableType;
+import javax.validation.executable.ValidateOnExecution;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 /**
@@ -17,5 +27,37 @@ import javax.ws.rs.Path;
 @Path("identification")
 @View("identification.jsp")
 public class IdentificationController {
+
+    @Inject // Le DAO généré par netBeans
+    ClientFacade dao;
+
+    @Inject
+    Models models;
     
+    @Inject // Les infos du joueur, Session scoped
+    private RechercheId resultat;
+
+
+    @GET
+    public void show() {
+        List<Client> toutlesclients = dao.findAll();
+        models.put("client", toutlesclients);
+    }
+
+    @POST
+    @ValidateOnExecution(type = ExecutableType.ALL)
+    public String login(@FormParam("contact") String contact) {
+        Client p = dao.find(contact);
+        try {
+            if (p != null) {
+                resultat.login(p.getContact();
+                return "ce contact existe";
+            } else {
+                models.put("databaseErrorMessage", "Ce contact n'existe pas");
+            }
+        } catch (Exception e) {
+            models.put("databaseErrorMessage", "Ce dispositif n'existe pas");
+        }
+        return null;
+    }
 }
